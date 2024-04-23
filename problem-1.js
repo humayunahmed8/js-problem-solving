31ffdafa774360e595b036e62f683b98386a588a
humayunahmed8 <humayunfarhad3@gmail.com>
2024-04-23 18:42:25 +0600

    Problem-1: Count occurrences and find first index of 'Bangladesh' in sentence

--- problem-1.js
+++ problem-1.js
@@ -1,5 +1,5 @@
 // নিচের Sentence টিতে Banagladesh শব্দটি কতবার ব্যবহার হয়েছে?
-// প্রথম Banagladesh শব্দটি কত নাম্বার index এ আছে 
+// প্রথম Banagladesh শব্দটি কত নাম্বার index এ আছে  
 
 const sentence = "In the heart of South Asia lies the vibrant nation of Bangladesh, where bustling cities and serene countryside harmoniously coexist. Bangladesh, with its rich cultural heritage and warm hospitality, beckons travelers to explore its diverse landscapes. From the lush greenery of the Sundarbans to the bustling streets of Dhaka, Bangladesh offers a tapestry of experiences. The people of Bangladesh are renowned for their resilience and determination, embodying the spirit of their nation. Whether savoring traditional Bengali cuisine or admiring the ancient architecture, every corner of Bangladesh tells a unique story of resilience and beauty.";
 
